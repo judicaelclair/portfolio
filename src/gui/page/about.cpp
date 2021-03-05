c3fd7d58aab231d0008dbf332c51456ae89d12b4
[ 大鳄 ] Asew <clair.judicael@gmail.com>
2021-03-05 10:17:57 +0000

    Updated content
    Signed-off-by: [ 大鳄 ] Asew <clair.judicael@gmail.com>

--- src/gui/page/about.cpp
+++ src/gui/page/about.cpp
@@ -4,7 +4,7 @@
  * permission from the copyright holder.
  * -----
  * File: /src/gui/page/about.cpp
- * Last Modified: Thursday, 4th March 2021 12:58:16 pm
+ * Last Modified: Friday, 5th March 2021 10:14:57 am
  * Modified By: Judicaël CLAIR <clair.judicael@gmail.com>
  */
 
@@ -37,8 +37,7 @@ void about_page::render_content_() {
                "provide disaster relief. To this end, I am currently developing context-aware prostheses for dexterous "
                "manipulation; with this technology later being adapted to humanoid robots.");
   ImGui::Dummy({0, 5});
-  wrapped_text("My areas of interest include robotics, augmented reality, and high-performance scientific computing "
-               "(e.g. fluid dynamics).");
+  wrapped_text("My current areas of interest include computer vision and robotics.");
   ImGui::Dummy({0, 30});
   ImGui::PushFont(semi_large_bold_font);
   { // CV link
